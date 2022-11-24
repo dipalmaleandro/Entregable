439f14592903633c97ab58b1c7311760086e2f9d
LeandroDiPalma <dipalmaleandro@yahoo.com.ar>
2022-11-24 19:11:33 -0300

    correccion 2

--- AppFamilia/views.py
+++ AppFamilia/views.py
@@ -4,22 +4,34 @@ from django.http import HttpResponse
 from django.template import Template,Context,loader
 
 # Create your views here.
-def integrantes(request):
-    persona=Integrantes(nombre="Leandro ", apellido="DiPalma ", documento="28711888", ocupacion="Administrativo")
-    persona.save()
-    cadena_texto=persona
+def integrante1(request):
+    persona1=Integrantes(nombre="Leandro ", apellido="DiPalma ", documento="28711888", ocupacion="Administrativo")
+    persona1.save()
+    cadena_texto=persona1
+    return HttpResponse(cadena_texto)
+
+def integrante2(request):
+    persona2=Integrantes(nombre="Eliana ", apellido="Alarcon ", documento="30911908", ocupacion="Estilista")
+    persona2.save()
+    cadena_texto=persona2
+    return HttpResponse(cadena_texto)
+
+def integrante3(request):
+    persona3=Integrantes(nombre="Santino ", apellido="DiPalma ", documento="40501888", ocupacion="Estudiante")
+    persona3.save()
+    cadena_texto=persona3
     return HttpResponse(cadena_texto)
 
 def familiahtml (request):
     archivo=open("template1.html")
     template=Template(archivo.read())
     archivo.close
-    contexto=Context(integrantes)
+    contexto=Context(Integrantes)
     documento=template.render(contexto)
     return HttpResponse(documento)
 
 def familia1html (request):
     plantilla=loader.get_template('template1.html')
-    documento=plantilla.render(integrantes)
+    documento=plantilla.render(Integrantes)
     return HttpResponse(documento)
 
