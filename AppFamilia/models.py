967f4a4e6064ef36a67d9bf970a733682a919191
LeandroDiPalma <dipalmaleandro@yahoo.com.ar>
2022-11-24 19:40:47 -0300

    clases

--- AppFamilia/models.py
+++ AppFamilia/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Integrantes(models.Model):
+class Integrantes1(models.Model):
+    nombre=models.CharField(max_length=250)
+    apellido=models.CharField(max_length=30)
+    documento=models.IntegerField()
+    ocupacion=models.CharField(max_length=30)
+    def __str__(self):
+        return self.nombre+" " + self.apellido+" "+ self.documento+" "+self.ocupacion
+class Integrantes2(models.Model):
+    nombre=models.CharField(max_length=250)
+    apellido=models.CharField(max_length=30)
+    documento=models.IntegerField()
+    ocupacion=models.CharField(max_length=30)
+    def __str__(self):
+        return self.nombre+" " + self.apellido+" "+ self.documento+" "+self.ocupacion
+class Integrantes3(models.Model):
     nombre=models.CharField(max_length=250)
     apellido=models.CharField(max_length=30)
     documento=models.IntegerField()
